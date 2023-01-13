ddb7cadd2235e7b24c8c91c5531d8436bbfc7f5e
Rakeshreddy sampathi <61946025+rakeshsampathi@users.noreply.github.com>
2023-01-13 16:40:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+const arr=str.split("");
+	var len=arr.length;
+	for(var i=0;i<len/2;i++){
+		if(arr[i]!=arr[len-1-i]){
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
